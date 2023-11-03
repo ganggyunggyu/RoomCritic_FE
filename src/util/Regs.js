8d2e534d63134865319da6e3b3a05f092867e58d
강경규 <qwzx16@naver.com>
2023-11-03 12:45:58 +0900

    FEAT: 날짜 변환 함수 추가 리뷰 작성 시간을 보여주기 위함입니다.

--- src/util/Regs.js
+++ src/util/Regs.js
@@ -2,5 +2,18 @@
 
 export const emailReg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
-export const passwordReg =
-  /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+export const passwordReg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+
+export const formatDateWithTime = (inputDateString) => {
+  const inputDate = new Date(inputDateString);
+
+  const year = inputDate.getFullYear();
+  const month = String(inputDate.getMonth() + 1).padStart(2, '0');
+  const day = String(inputDate.getDate()).padStart(2, '0');
+  const hours = String(inputDate.getHours()).padStart(2, '0');
+  const minutes = String(inputDate.getMinutes()).padStart(2, '0');
+
+  const formattedDate = `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
+
+  return formattedDate;
+};
