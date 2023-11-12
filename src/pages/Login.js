7b220d128f968fc747389461a2aa0103bb12ee31
강경규 <qwzx16@naver.com>
2023-11-12 23:23:26 +0900

    FEAT: Kakao로그인 버튼 , 기능 추가
    -미완

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,10 @@
-/** @format */
-
 import React, { useState } from 'react';
 import FormHeader from '../components/FormHeader';
 import FormInput from '../components/FormInput';
 import FormButton from '../components/FormButton';
 import axiosConfig from '../api/axiosConfig';
 import { useNavigate } from 'react-router-dom';
+import { KAKAOREST } from '../config';
 
 import { userInfoState, isLoggedInState } from '../recoilAtoms';
 import { useRecoilState } from 'recoil';
@@ -46,10 +45,17 @@ export default function Login() {
     navigator('/join');
   };
 
+  const isKakaoLogin = () => {
+    const redirect_uri = 'http://localhost:3000/auth'; //Redirect URI
+    // oauth 요청 URL
+    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAOREST}&redirect_uri=${redirect_uri}&response_type=code&scope=friends`;
+    window.location.href = kakaoURL;
+  };
+
   return (
-    <div className='flex flex-col h-screen justify-center items-center'>
+    <div className="flex flex-col h-screen justify-center items-center">
       <FormHeader text={'로그인'} />
-      <form className='w-5/6 md:w-80 mt-5'>
+      <form className="w-5/6 md:w-80 mt-5">
         {FormItems.map((el, i) => {
           return (
             <FormInput
@@ -63,6 +69,7 @@ export default function Login() {
         })}
         <FormButton submitFunc={isLogin} text={'로그인'} />
         <FormButton submitFunc={isJoin} text={'회원가입'} />
+        <FormButton submitFunc={isKakaoLogin} text={'카카오 로그인'} />
       </form>
     </div>
   );
