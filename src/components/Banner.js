1d3e552d6cf09985f2115c4ffc4c89feb4516f93
강경규 <qwzx16@naver.com>
2024-02-01 23:58:55 +0900

    FEAT: 베너 테스트 , 이미지 추가

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Banner() {
   return (
     <div className="flex justify-around bg-red-400 text-slate-50 w-screen">
-      <div className="h-72 text-xl flex flex-col justify-center flex-none gap-3 transition-all">
-        <p className="text-3xl">나도 집에서 영화만 보면서 말이야</p>
-        <p>평가만 하면서말야</p>
-        <p>돈 벌고싶다 이말이야</p>
+      <div className="w-7/12 h-72 text-xl flex flex-col justify-center flex-none gap-3 transition-all">
+        <p className="text-3xl">Room Critic = 방구석 평론가</p>
+        <p>Room = 방</p>
+        <p>Critic = 평론가</p>
       </div>
     </div>
   );
