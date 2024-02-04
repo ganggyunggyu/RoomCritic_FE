6f7ffb530645361d1d2c271b11dba6c3f691d67d
강경규 <qwzx16@naver.com>
2024-02-04 18:27:23 +0900

    STYLE: 배너 텍스트 변경

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Banner() {
   return (
     <div className="flex justify-around bg-red-400 text-slate-50 w-screen">
-      <div className="w-7/12 h-72 text-xl flex flex-col justify-center flex-none gap-3 transition-all">
-        <p className="text-3xl">Room Critic = 방구석 평론가</p>
-        <p>Room = 방</p>
-        <p>Critic = 평론가</p>
+      <div className="md:w-7/12 w-10/12 md:h-60 py-3 flex flex-col justify-center gap-3 transition-all">
+        <p className="md:text-3xl text-md">Room Critic = 방구석 평론가</p>
+        <p className="text-sm md:text-lg">Room = 방</p>
+        <p className="text-sm md:text-lg">Critic = 평론가</p>
       </div>
     </div>
   );
