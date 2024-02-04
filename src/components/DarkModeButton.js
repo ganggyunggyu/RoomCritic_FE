c76db1adea25ab987ceabd75a04db8deb6e75b0a
강경규 <qwzx16@naver.com>
2024-02-04 18:27:43 +0900

    STYLE: 다크모드 버튼우선순위 변경

--- src/components/DarkModeButton.js
+++ src/components/DarkModeButton.js
@@ -5,7 +5,7 @@ export default function DarkModeButton({ darkMode, toggleDarkMode }) {
   return (
     <button
       onClick={toggleDarkMode}
-      className="h-12 w-12 border rounded-full flex items-center justify-center fixed bottom-8 right-8"
+      className="h-12 w-12 border rounded-full flex items-center justify-center fixed bottom-8 right-8 z-20"
     >
       {darkMode ? <Sun /> : <Moon />}
     </button>
