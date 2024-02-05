d915bc0008e2c1d50bf0af09be23a3057d1153a4
강경규 <qwzx16@naver.com>
2024-02-05 14:31:51 +0900

    CSS: 콘텐츠 상세페이지 버튼 반응형 정렬

--- src/pages/ContentDetail.js
+++ src/pages/ContentDetail.js
@@ -3,17 +3,19 @@ import ContentInfo from '../components/ContentDetail/ContentInfo';
 import { useNavigate, useParams } from 'react-router-dom';
 import ResponsiveProvider from '../components/WrapProvider/ResponsiveProvider';
 import useContentFetch from '../hooks/useContentFetch';
-
 import CardWrapProvider from '../components/WrapProvider/CardWrapProvider';
-
 import Footer from '../components/Footer';
 import Contents from './Contents';
 import DetailBackground from '../components/DetailBackground';
 import Button from '../components/AtomComponent/Button';
+import { useRecoilValue } from 'recoil';
+import { isLoggedInState } from '../recoilAtoms';
+
 export default function ContentDetail() {
   const navigator = useNavigate();
   const { mediaType, contentId } = useParams();
   const { content, fetchContent, reviews, fetchReview } = useContentFetch(mediaType, contentId);
+  const isLoggedIn = useRecoilValue(isLoggedInState);
 
   const redirectReview = (review) => {
     navigator(`/detail/review/${review.userId}/${review._id}`);
@@ -32,18 +34,27 @@ export default function ContentDetail() {
     <React.Fragment>
       <DetailBackground path={content.backdrop_path} />
       <ContentInfo content={content} />
-      <ResponsiveProvider direction={'row'} className={'gap-5 z-10'}>
-        <Button label={'좋아요 🤩'} bg={'main'} className={'w-4/12 md:text-lg text-xs'} />
-        <Button label={'별로에요 🧐'} bg={'main'} className={'w-4/12 md:text-lg text-xs'} />
-        <Button
-          label={'리뷰 쓰러가기'}
-          bg={'main'}
-          className={'w-4/12 md:text-lg text-xs'}
-          onClick={() => navigator(`/create/${mediaType}/${contentId}`)}
-        />
+      <ResponsiveProvider direction={'col'} className={'gap-5 z-10 lg:flex-row'}>
+        <Button label={'좋아요 🤩'} bg={'main'} className={'lg:w-4/12 w-2/3 text-lg'} />
+        <Button label={'별로에요 🧐'} bg={'main'} className={'lg:w-4/12 w-2/3 text-lg'} />
+        {isLoggedIn ? (
+          <Button
+            label={'리뷰 쓰러가기'}
+            bg={'main'}
+            className={'lg:w-4/12 w-2/3 text-lg'}
+            onClick={() => navigator(`/create/${mediaType}/${contentId}`)}
+          />
+        ) : (
+          <Button
+            label={'로그인하고 리뷰쓰자!'}
+            bg={'main'}
+            className={'lg:w-4/12 w-2/3 text-lg'}
+            onClick={() => navigator(`/login`)}
+          />
+        )}
       </ResponsiveProvider>
       {reviews.length === 0 ? (
-        <p className="py-10">남겨진 리뷰가 없어요 🥲</p>
+        <p className='py-10'>남겨진 리뷰가 없어요 🥲</p>
       ) : (
         <CardWrapProvider
           title={`${content.title || content.name}에 남겨진 리뷰`}
