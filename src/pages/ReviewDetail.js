b4c90aec4020356a971338d9c09e4699eaa74332
강경규 <qwzx16@naver.com>
2024-02-05 11:02:43 +0900

    STYLE: css 수정

--- src/pages/ReviewDetail.js
+++ src/pages/ReviewDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { reviewsState, userInfoState } from '../recoilAtoms';
 import { seletReview } from '../recoilAtoms';
@@ -15,7 +15,7 @@ import ResponsiveProvider from '../components/WrapProvider/ResponsiveProvider';
 export default function ReviewDetail() {
   const reviews = useRecoilValue(reviewsState);
   const { userId, reviewId } = useParams();
-
+  const navigator = useNavigate();
   const [selectReview, setSelectReview] = useRecoilState(seletReview);
   const user = useRecoilValue(userInfoState);
   //클라이언트에 리뷰 데이터가 있다면 그것을 사용하고 그렇지 않을 시 서버요청을 하고싶음
@@ -56,12 +56,13 @@ export default function ReviewDetail() {
       <div className="gap-10 pt-10 flex items-center justify-center">
         <div className="flex flex-col justify-center items-center gap-10 z-10">
           <h1
-            className="text-2xl"
+            className="text-2xl cursor-pointer hover:text-red-400"
             onClick={() => {
               console.log(selectReview);
+              navigator(`/detail/${selectReview.contentType}/${selectReview.contentId}`);
             }}
           >
-            {selectReview.contentName}
+            {selectReview.contentName} 다른 리뷰도 보러가요
           </h1>
           <p>{selectReview.userName}님의 리뷰</p>
           <p>{formatDateWithTime(selectReview.createTime)}</p>
@@ -74,14 +75,13 @@ export default function ReviewDetail() {
           <p>평점은 {selectReview.grade}점 드립니다</p>
         </div>
       </div>
-      <ResponsiveProvider direction={'row'} className={'gap-5 z-10'}>
-        <Button label={'좋아요 🤩'} bg={'main'} className={'w-4/12 text-lg'} />
-        <Button label={'별로에요 🧐'} bg={'main'} className={'w-4/12 text-lg'} />
-        {/* <Button label={'한줄평 쓰러가기'} bg={'main'} className={'w-4/12 text-lg'} /> */}
+      <ResponsiveProvider direction={'col'} className={'gap-5 z-10 lg:flex-row transition-all'}>
+        <Button label={'좋아요 🤩'} bg={'main'} className={'lg:w-4/12 w-2/3 text-lg'} />
+        <Button label={'별로에요 🧐'} bg={'main'} className={'lg:w-4/12 w-2/3 text-lg'} />
         {user._id === selectReview.userId && (
           <>
-            <Button label={'삭제'} bg={'main'} className={'w-4/12 text-lg'} />
-            <Button label={'수정'} bg={'main'} className={'w-4/12 text-lg'} />
+            <Button label={'삭제'} bg={'main'} className={'lg:w-4/12 w-2/3 text-lg'} />
+            <Button label={'수정'} bg={'main'} className={'lg:w-4/12 w-2/3 text-lg'} />
           </>
         )}
       </ResponsiveProvider>
