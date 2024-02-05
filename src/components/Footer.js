60f6a46d957e1cadc3b56bbe00f063baaa8e9f20
강경규 <qwzx16@naver.com>
2024-02-05 19:36:20 +0900

    FEAT: 푸터내용추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,18 @@ import ResponsiveProvider from './WrapProvider/ResponsiveProvider';
 
 export default function Footer() {
   return (
-    <ResponsiveProvider direction={'col'} className={'gap-5 p-10 opacity-60'}>
-      <p>develop by 강경규</p>
-      <p className="text-xl">connect</p>
-      <p>qwzx16@naver.com</p>
+    <ResponsiveProvider direction={'row'} className={'gap-5 p-10 opacity-60 text-sm'}>
+      <div className='flex flex-col gap-5 items-center justify-center'>
+        <p>develop by 강경규</p>
+        <p>connect</p>
+        <p>qwzx16@naver.com</p>
+        <a href='https://github.com/ganggyunggyu' className='text-green-300 hover:text-green-700'>
+          GitHub
+        </a>
+        <a href='https://velog.io/@qwzx16/posts' className='text-green-300 hover:text-green-700'>
+          Blog
+        </a>
+      </div>
     </ResponsiveProvider>
   );
 }
