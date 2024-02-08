3073da1eaf65294ffaad40fd886e1566844ef428
강경규 <qwzx16@naver.com>
2024-02-08 17:25:47 +0900

    FEAT: 카드 배열에 따른 분기 임시 추가

--- src/components/WrapProvider/CardWrapProvider.js
+++ src/components/WrapProvider/CardWrapProvider.js
@@ -20,64 +20,71 @@ const CardWrapProvider = ({ title, cardList, onClick }) => {
 
   return (
     <ResponsiveProvider direction={'row'}>
-      <div className='w-full z-10 relative'>
-        <div className='absolute inset-y-0 left-0 z-20 rounded-full opacity-50 flex items-center justify-center hover:opacity-80 transition-all'>
-          <button
-            onClick={() => {
-              handleScroll('left');
-            }}
-            className='w-12 h-12 bg-slate-500 rounded-full flex items-center justify-center'
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='w-8 h-8'
+      {!cardList && '작품이 없습니다.'}
+      {!cardList.length && '작품이 없습니다.'}
+      {cardList.length > 0 && (
+        <div className='w-full z-10 relative'>
+          <div className='absolute inset-y-0 left-0 z-20 rounded-full opacity-50 flex items-center justify-center hover:opacity-80 transition-all'>
+            <button
+              onClick={() => {
+                handleScroll('left');
+              }}
+              className='w-12 h-12 bg-slate-500 rounded-full flex items-center justify-center'
             >
-              <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5 8.25 12l7.5-7.5' />
-            </svg>
-          </button>
-        </div>
-        <div className='absolute inset-y-0 right-0 z-20 rounded-full opacity-50 flex items-center justify-center hover:opacity-80 transition-all'>
-          <button
-            onClick={() => {
-              handleScroll('right');
-            }}
-            className='w-12 h-12 bg-slate-500 rounded-full flex items-center justify-center'
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='w-8 h-8'
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-8 h-8'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M15.75 19.5 8.25 12l7.5-7.5'
+                />
+              </svg>
+            </button>
+          </div>
+          <div className='absolute inset-y-0 right-0 z-20 rounded-full opacity-50 flex items-center justify-center hover:opacity-80 transition-all'>
+            <button
+              onClick={() => {
+                handleScroll('right');
+              }}
+              className='w-12 h-12 bg-slate-500 rounded-full flex items-center justify-center'
             >
-              <path strokeLinecap='round' strokeLinejoin='round' d='m8.25 4.5 7.5 7.5-7.5 7.5' />
-            </svg>
-          </button>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-8 h-8'
+              >
+                <path strokeLinecap='round' strokeLinejoin='round' d='m8.25 4.5 7.5 7.5-7.5 7.5' />
+              </svg>
+            </button>
+          </div>
+          <h1 className='md:text-3xl text-lg'>{title}</h1>
+          <div
+            ref={cardConatinerRef}
+            className='flex overflow-x-scroll overflow-y-hidden gap-5 py-5 smooth-scroll'
+          >
+            {cardList.map((content, i) => {
+              return (
+                <Card
+                  key={content.id || content._id}
+                  content={content}
+                  onClick={() => {
+                    onClick(content);
+                  }}
+                />
+              );
+            })}
+          </div>
         </div>
-
-        <h1 className='md:text-3xl text-lg'>{title}</h1>
-        <div
-          ref={cardConatinerRef}
-          className='flex overflow-x-scroll overflow-y-hidden gap-5 py-5 smooth-scroll'
-        >
-          {cardList.map((content, i) => {
-            return (
-              <Card
-                key={content.id || content._id}
-                content={content}
-                onClick={() => {
-                  onClick(content);
-                }}
-              />
-            );
-          })}
-        </div>
-      </div>
+      )}
     </ResponsiveProvider>
   );
 };
