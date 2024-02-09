178ba9022306fde367bc51f5815c18ba01d702fb
강경규 <qwzx16@naver.com>
2024-02-09 22:40:05 +0900

    REFACTOR: 선택한 리뷰 불러오기 Query로 리펙토링

---
+++ src/hooks/useReviewSelect.js
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import axiosConfig from '../api/axiosConfig';
+const fetchSelectReview = async (userId, reviewId) => {
+  const result = await axiosConfig.get(`review/detail/${userId}/${reviewId}`);
+  return result;
+};
+
+const useReviewSelect = (userId, reviewId) => {
+  const selectReviewQuery = useQuery({
+    queryKey: ['selectReview', userId, reviewId],
+    queryFn: () => fetchSelectReview(userId, reviewId),
+  });
+  return { selectReviewQuery };
+};
+
+export default useReviewSelect;
