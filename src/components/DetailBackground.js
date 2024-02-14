38363a3ae434db06437fc4123650b3428bc2ef1f
강경규 <qwzx16@naver.com>
2024-02-14 17:48:26 +0900

    FEAT: 배경 레이지로딩 적용

--- src/components/DetailBackground.js
+++ src/components/DetailBackground.js
@@ -2,16 +2,17 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 const DetailBackground = ({ path }) => {
   return (
-    <img
-      className='fixed top-0 opacity-20 z-0 blur-sm w-full h-full'
-      src={`https://www.themoviedb.org/t/p/original/${path}`}
-      alt={path}
-      loading='lazy'
-      // effect="blur"
-      // width={'100vw'}
-      // height={'100vh'}
-      // style={{ position: 'fixed' }}
-    />
+    <div className='fixed top-0 opacity-20 z-0 blur-sm w-screen h-screen'>
+      <LazyLoadImage
+        src={`https://www.themoviedb.org/t/p/original/${path}`}
+        alt={path}
+        loading='lazy'
+        effect='blur'
+        width={'100%'}
+        height={'100%'}
+        style={{ position: 'fixed' }}
+      />
+    </div>
   );
 };
 export default DetailBackground;
