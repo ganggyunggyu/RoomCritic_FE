fa59aca17e4d527e7cd273dca0dccd22a0781b22
강경규 <qwzx16@naver.com>
2024-02-16 01:47:43 +0900

    FEAT: hover state 추가 tailwind -> Vanila css 변경

--- src/components/Banner/BannerReview.js
+++ src/components/Banner/BannerReview.js
@@ -4,9 +4,18 @@ import { cn } from '../../util/cn';
 
 const BannerReview = ({ review }) => {
   const [movieColor, setMovieColor] = React.useState(review.movie_color);
+  const [hoverColor, setHoverColor] = React.useState(review.hover_color);
+  const [isHovering, setIsHovering] = React.useState(false);
+
   if (movieColor !== review.movie_color) {
     setMovieColor(review.movie_color);
+    setHoverColor(review.hover_color);
   }
+
+  // React.useEffect(() => {
+  //   setMovieColor(review.movie_color);
+  //   console.log(review);
+  // }, [review]);
   return (
     <React.Fragment>
       <div
@@ -16,7 +25,10 @@ const BannerReview = ({ review }) => {
       text-viloet-300 text-rose-300 text-green-300 hover:text-rose-700 hover:text-green-700 hover:text-violet-700'
       />
       <div className='md:w-7/12 w-10/12 flex flex-col justify-center gap-1 md:gap-3 transition-all max-w-fit absolute top-0 bottom-0 left-12 md:left-40'>
-        <p className={cn(`md:text-5xl text-xl transition-1s text-${movieColor}-300`)}>
+        <p
+          style={{ color: movieColor }}
+          className={cn(`md:text-5xl text-xl transition-1s text-${movieColor}-300`)}
+        >
           {review.title}
         </p>
         {review.lines.map((line, i) => {
@@ -27,10 +39,17 @@ const BannerReview = ({ review }) => {
           );
         })}
         <Link
+          style={{ color: isHovering ? hoverColor : movieColor }}
           className={cn(
             `transition-all py-1 md:text-3xl text-${movieColor}-300 hover:text-${movieColor}-700`,
           )}
           to={review.link}
+          onMouseEnter={() => {
+            setIsHovering(!isHovering);
+          }}
+          onMouseLeave={() => {
+            setIsHovering(!isHovering);
+          }}
         >
           👉 {review.title} 리뷰 쓰러가기
         </Link>
