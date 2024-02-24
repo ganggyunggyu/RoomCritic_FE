b5c11cadb2179c04085fae519b6c6b6046af0cc8
강경규 <qwzx16@naver.com>
2024-02-24 19:06:46 +0900

    CSS: 다크모드 글씨 색상 변경 접근성 점수수

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ResponsiveProvider from './WrapProvider/ResponsiveProvider';
-
+import useDarkMode from '../hooks/useDarkMode';
 export default function Footer() {
+  const { isDarkMode } = useDarkMode();
   return (
     <ResponsiveProvider direction={'col'} className={'gap-5 p-10 opacity-60 text-sm'}>
       <p>develop by 강경규</p>
       <p>connect</p>
       <p>qwzx16@naver.com</p>
-      <a href='https://github.com/ganggyunggyu' className='text-green-300 hover:text-green-700'>
+      <a
+        href='https://github.com/ganggyunggyu'
+        className={`text-green-500 hover:text-green-700 ${isDarkMode && 'text-green-300'}`}
+      >
         GitHub
       </a>
-      <a href='https://velog.io/@qwzx16/posts' className='text-green-300 hover:text-green-700'>
+      <a
+        href='https://velog.io/@qwzx16/posts'
+        className={`text-green-500 hover:text-green-700 ${isDarkMode && 'text-green-300'}`}
+      >
         Blog
       </a>
     </ResponsiveProvider>
