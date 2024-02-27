c2c08e7a1d4a8fa1f96a2efa0d356833608bec5d
강경규 <qwzx16@naver.com>
2024-02-27 23:07:14 +0900

    FEAT: 카드에 정보 추가

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,10 +3,12 @@ import CardImage from './CardImage';
 import CardHover from './CardHover';
 import { cva } from 'class-variance-authority';
 import { cn } from '../../util/cn';
+import { formatDateWithTime } from '../../util/Regs';
 
 export const CardWrapVariants = cva(``);
 
 export default function Card({ content, onClick, isHover }) {
+  const { formattedMonthEnd } = formatDateWithTime(content.release_date);
   const [cardHover, setCardHover] = useState(false);
   const cardMouseOver = () => {
     setCardHover(true);
@@ -27,7 +29,20 @@ export default function Card({ content, onClick, isHover }) {
         <CardImage
           path={content.contentPosterImg || content.poster_path || content.backdrop_path}
         />
-        <p>{content.title || content.name || content.contentName}</p>
+        <p className='whitespace-nowrap	overflow-hidden text-ellipsis	'>
+          {content.title || content.contentName}
+        </p>
+        {content.vote_average && (
+          <React.Fragment>
+            <p>
+              회원 점수{' '}
+              <span className='text-yellow-400'>{Math.round(content.vote_average * 10)}%</span>
+            </p>
+            <p>
+              개봉 날짜 <span className='text-violet-400'>{formattedMonthEnd}</span>
+            </p>
+          </React.Fragment>
+        )}
       </div>
     </div>
   );
