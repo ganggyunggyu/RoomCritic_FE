19c23d86bc9b8f06cc73489124bdf414380caca9
강경규 <qwzx16@naver.com>
2024-02-27 23:02:53 +0900

    FEAT: 카테고리 별 영화 콘텐츠 보여주는 쿼리 추가

--- src/components/CategoryReviewList.js
+++ src/components/CategoryReviewList.js
@@ -2,58 +2,139 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { isLoggedInState, searchContentsState } from '../recoilAtoms';
-import useReviewFetch from '../hooks/review/useReviewFetch';
 import CardWrapProvider from './WrapProvider/CardWrapProvider';
 import Footer from './Footer';
 import Loading from './Loading';
+import axiosConfig from '../api/axiosConfig';
+import { useQuery } from '@tanstack/react-query';
+import ResponsiveProvider from './WrapProvider/ResponsiveProvider';
+
+const fetchLatestTvContent = async () => {
+  const result = await axiosConfig.get('tv/latest');
+  return result;
+};
+const fetchLatestMovieContent = async () => {
+  const result = await axiosConfig.get('movie/latest');
+  return result;
+};
+
+const fetchOwnerPickTvContent = async () => {
+  const result = await axiosConfig.get('tv/owner');
+  return result;
+};
+const fetchOwnerPickMovieContent = async () => {
+  const result = await axiosConfig.get('movie/owner');
+  return result;
+};
+const fetchTopRatedMovie = async () => {
+  const result = await axiosConfig.get('movie/top-rated-movies');
+  return result;
+};
 
 const CategoryReviewList = () => {
   const navigator = useNavigate();
   const isLoggedIn = useRecoilValue(isLoggedInState);
   const searchContents = useRecoilValue(searchContentsState);
-  const { latestReviewsQuery, tvContentReviewsQuery, movieContentReviewsQuery } = useReviewFetch();
+
   const redirectContent = (content) => {
     window.scrollTo(0, 0);
-    navigator(`/detail/${content.media_type}/${content.id}`);
-  };
-  const redirectReview = (review) => {
-    window.scrollTo(0, 0);
-    navigator(`/detail/review/${review.userId}/${review._id}`);
+    navigator(`/content/${content.content_type}/${content._id}`);
   };
 
+  const latestTvContentQuery = useQuery({
+    queryKey: ['latestTvContent'],
+    queryFn: fetchLatestTvContent,
+  });
+  const latestMovieContentQuery = useQuery({
+    queryKey: ['latestMovieContent'],
+    queryFn: fetchLatestMovieContent,
+  });
+
+  const ownerPickMovieQuery = useQuery({
+    queryKey: ['ownerMovieContent'],
+    queryFn: fetchOwnerPickMovieContent,
+  });
+
+  const ownerPickTvQuery = useQuery({
+    queryKey: ['ownerTvContent'],
+    queryFn: fetchOwnerPickTvContent,
+  });
+  const topRatedMovieQuery = useQuery({
+    queryKey: ['topRatedMovie'],
+    queryFn: fetchTopRatedMovie,
+  });
+
+  const { isLoading: isLatestMovieLoading, data: latestMovieData } = latestMovieContentQuery;
+  const { isLoading: isLatestTvLoading, data: latestTvData } = latestTvContentQuery;
+  const { isLoading: isOwnerPickMovieLoading, data: ownerPickMovieData } = ownerPickMovieQuery;
+  const { isLoading: isOwnerPickTvLoading, data: ownerPickTvData } = ownerPickTvQuery;
+  const { isLoading: isTopRatedMovieLoading, data: topRatedMovieData } = topRatedMovieQuery;
+
   if (
-    latestReviewsQuery.isPending ||
-    tvContentReviewsQuery.isPending ||
-    movieContentReviewsQuery.isPending
+    isLatestMovieLoading &&
+    isLatestTvLoading &&
+    isOwnerPickMovieLoading &&
+    isOwnerPickTvLoading &&
+    isTopRatedMovieLoading &&
+    !searchContents
   ) {
     return <Loading />;
   }
-  return (
-    <React.Fragment>
-      {isLoggedIn && searchContents.length !== 0 && (
-        <CardWrapProvider
-          title={'최근에 검색한 작품'}
-          cardList={searchContents}
-          onClick={redirectContent}
-        />
-      )}
-      <CardWrapProvider
-        title={'최근에 작성된 리뷰들'}
-        cardList={latestReviewsQuery.data.data.reviews}
-        onClick={redirectReview}
-      />
-      <CardWrapProvider
-        title={'TV 시리즈 리뷰들'}
-        cardList={tvContentReviewsQuery.data.data.tvContentReviews}
-        onClick={redirectReview}
-      />
-      <CardWrapProvider
-        title={'영화 리뷰들'}
-        cardList={movieContentReviewsQuery.data.data.movieContentReviews}
-        onClick={redirectReview}
-      />
-      <Footer />
-    </React.Fragment>
-  );
+
+  if (
+    !isLatestMovieLoading &&
+    !isLatestTvLoading &&
+    !isOwnerPickMovieLoading &&
+    !isOwnerPickTvLoading &&
+    !isTopRatedMovieLoading &&
+    searchContents
+  ) {
+    const latestMovies = latestMovieData?.data?.movies || [];
+    const latestTvs = latestTvData?.data?.tvs || [];
+    const ownerMovies = ownerPickMovieData?.data?.movies || [];
+    const ownerTvs = ownerPickTvData?.data?.tvs || [];
+    const topRatedMovies = topRatedMovieData?.data?.movies || [];
+
+    return (
+      <ResponsiveProvider direction={'col'}>
+        {isLoggedIn && searchContents.length !== 0 && (
+          <CardWrapProvider
+            title={'최근 검색 결과'}
+            cardList={searchContents}
+            onClick={redirectContent}
+          />
+        )}
+        {ownerMovies && ownerTvs && (
+          <CardWrapProvider
+            title={'주인장 선정 작품입니다!'}
+            cardList={[...ownerMovies, ...ownerTvs]}
+            onClick={redirectContent}
+          />
+        )}
+
+        {latestMovies && (
+          <CardWrapProvider
+            title={'최근에 평론을 받은 영화에요!'}
+            cardList={latestMovies}
+            onClick={redirectContent}
+          />
+        )}
+        {latestTvs && (
+          <CardWrapProvider
+            title={'최근에 평론을 받은 프로그램이에요!'}
+            cardList={latestTvs}
+            onClick={redirectContent}
+          />
+        )}
+        {topRatedMovies && (
+          <CardWrapProvider
+            title={'길이 남을 명작들을 가져왔어요'}
+            cardList={topRatedMovies}
+            onClick={redirectContent}
+          />
+        )}
+      </ResponsiveProvider>
+    );
+  }
 };
 export default CategoryReviewList;
