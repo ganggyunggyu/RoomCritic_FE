715b48c26d67d93c3922676ac8b35740880589a7
강경규 <qwzx16@naver.com>
2024-02-27 23:10:10 +0900

    FEAT: contentType 별 검색기능 추가

--- src/hooks/content/useSearchContents.js
+++ src/hooks/content/useSearchContents.js
@@ -1,24 +1,46 @@
 import { useQuery } from '@tanstack/react-query';
 import { useSetRecoilState } from 'recoil';
-import tmdbAxiosConfig from '../../api/tmdbAxiosConfig';
+
 import { searchContentsState } from '../../recoilAtoms';
+
+import axiosConfig from '../../api/axiosConfig';
 
 const useSearchContents = (searchValue) => {
   const setSerchContents = useSetRecoilState(searchContentsState);
 
   const fetchSearchContents = async () => {
-    const result = await tmdbAxiosConfig.get(
-      `/search/multi?include_adult=false&query=${searchValue}`,
-    );
-    setSerchContents(result.data.results);
+    const result = await axiosConfig.get(`content/search?search_value=${searchValue}`);
+
+    setSerchContents(result.data.contents);
     return result;
   };
+  const fetchSearchTvContents = async () => {
+    const result = await axiosConfig.get(`content/search/tv?search_value=${searchValue}`);
+
+    setSerchContents(result.data.contents);
+    return result;
+  };
+  const fetchSearchMovieContents = async () => {
+    const result = await axiosConfig.get(`content/search/movie?search_value=${searchValue}`);
+
+    setSerchContents(result.data.contents);
+    return result;
+  };
+
   const searchContentsQuery = useQuery({
     queryKey: ['searchContents', searchValue],
     queryFn: fetchSearchContents,
   });
+  const searchTvContentsQuery = useQuery({
+    queryKey: ['searchTvContents', searchValue],
+    queryFn: fetchSearchTvContents,
+  });
+  const searchMovieContentsQuery = useQuery({
+    queryKey: ['searchMovieContents', searchValue],
+    queryFn: fetchSearchMovieContents,
+  });
 
-  return { searchContentsQuery };
+  return { searchContentsQuery, searchTvContentsQuery, searchMovieContentsQuery };
 };
 
 export default useSearchContents;
--- src/pages/Serch.js
+++ src/pages/Serch.js
@@ -9,59 +9,102 @@ import useSearchContents from '../hooks/content/useSearchContents';
 import Loading from '../components/Loading';
 export default function Serch() {
   const [searchValue, setSerchValue] = useState('');
-  const { searchContentsQuery } = useSearchContents(searchValue);
+  const [searchType, setSearchType] = useState(0);
+  //0 전체 1 영화 2 티비
+  const { searchContentsQuery, searchMovieContentsQuery, searchTvContentsQuery } =
+    useSearchContents(searchValue);
   const isSearchValue = searchValue.length === 0;
 
   const navigator = useNavigate();
 
-  const isDetailReview = (content) => {
-    navigator(`/detail/${content.media_type}/${content.id}`);
+  const directDetailContent = (content) => {
+    navigator(`/content/${content.content_type}/${content._id}`);
   };
 
   return (
     <React.Fragment>
-      <ResponsiveProvider direction={'col'}>
-        <div className='w-2/3 relative'>
-          <Input
-            value={searchValue}
-            onChange={(e) => {
-              setSerchValue(e.target.value);
-            }}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                e.preventDefault();
-                searchContentsQuery.refetch();
-              }
-            }}
-            name={'search'}
-            className='w-full'
+      <ResponsiveProvider direction={'col'} className={'gap-5'}>
+        <div className='flex gap-3 w-2/3'>
+          <Button
+            label={'전체 검색'}
+            bg={searchType === 0 ? 'main' : 'default'}
+            onClick={() => setSearchType(0)}
           />
           <Button
-            item={<SerchIcon />}
-            bg={'main'}
-            className={'absolute right-0 top-0'}
-            onClick={searchContentsQuery.refetch}
+            label={'영화 검색'}
+            bg={searchType === 1 ? 'main' : 'default'}
+            onClick={() => setSearchType(1)}
+          />
+          <Button
+            label={'TV 검색'}
+            bg={searchType === 2 ? 'main' : 'default'}
+            onClick={() => setSearchType(2)}
           />
         </div>
-      </ResponsiveProvider>
-      {isSearchValue ? (
-        <React.Fragment>
-          <p className='pt-14 text-8xl animate-bounce pointer-events-none'>👆</p>
-          <p className='text-xl'>원하는 작품을 검색해보세요!</p>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          {searchContentsQuery.isLoading ? (
-            <Loading />
-          ) : (
-            <CardWrapProvider
-              title={'검색결과'}
-              cardList={searchContentsQuery.data.data.results}
-              onClick={isDetailReview}
+        <div className='w-2/3 relative'>
+          {searchType === 0 && (
+            <Input
+              label={'영화 & TV프로그램 검색'}
+              value={searchValue}
+              onChange={(e) => {
+                setSerchValue(e.target.value);
+              }}
+              name={'search'}
+              className='w-full'
             />
           )}
-        </React.Fragment>
-      )}
+          {searchType === 1 && (
+            <Input
+              label={'영화 검색'}
+              value={searchValue}
+              onChange={(e) => {
+                setSerchValue(e.target.value);
+              }}
+              name={'search'}
+              className='w-full'
+            />
+          )}
+          {searchType === 2 && (
+            <Input
+              label={'TV 프로그램 검색'}
+              value={searchValue}
+              onChange={(e) => {
+                setSerchValue(e.target.value);
+              }}
+              name={'search'}
+              className='w-full'
+            />
+          )}
+        </div>
+        {isSearchValue ? (
+          <React.Fragment>
+            <p className='pt-14 text-8xl animate-bounce pointer-events-none'>👆</p>
+            <p className='text-xl'>원하는 작품을 검색해보세요!</p>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            {searchContentsQuery.isLoading ||
+            searchMovieContentsQuery.isLoading ||
+            searchTvContentsQuery.isLoading ? (
+              <Loading />
+            ) : (
+              <CardWrapProvider
+                title={'검색 결과'}
+                cardList={
+                  searchType === 0
+                    ? searchContentsQuery.data.data.contents // searchType이 0일 때의 데이터
+                    : searchType === 1
+                    ? searchMovieContentsQuery.data.data.contents // searchType이 1일 때의 데이터
+                    : searchType === 2
+                    ? searchTvContentsQuery.data.data.contents // searchType이 2일 때의 데이터
+                    : [] // 기본값
+                }
+                onClick={directDetailContent}
+              />
+            )}
+          </React.Fragment>
+        )}
+      </ResponsiveProvider>
     </React.Fragment>
   );
 }
