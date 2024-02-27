77b9a60da5bbe84f57200481861f2fbcefcce54e
강경규 <qwzx16@naver.com>
2024-02-27 23:10:31 +0900

    FEAT: MyPage 기능 추가
    - 장르별 점수 배점
    - 임시 프로필
    - 글 쓴 갯수 확인 가능

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -8,27 +8,114 @@ import { useNavigate, useParams } from 'react-router-dom';
 import CardWrapProvider from '../components/WrapProvider/CardWrapProvider';
 import useMyReviewFetch from '../hooks/review/useMyReviewFetch';
 import Loading from '../components/Loading';
+import ResponsiveProvider from '../components/WrapProvider/ResponsiveProvider';
+import axiosConfig from '../api/axiosConfig';
+import { useQuery } from '@tanstack/react-query';
+import Button from '../components/AtomComponent/Button';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import SettingIcon from '../icons/SettingIcon';
 
 export default function MyPage() {
   const { userId } = useParams();
   const navigator = useNavigate();
   const userInfo = useRecoilValue(userInfoState);
   const { myReviewQuery } = useMyReviewFetch(userId);
-
+  const { isLoading: isMyReviewLoading, data: myReviewData } = myReviewQuery;
   const redirectReview = (review) => {
     navigator(`/detail/review/${review.userId}/${review._id}`);
   };
-  return (
-    <React.Fragment>
-      {myReviewQuery.isLoading ? (
-        <Loading />
-      ) : (
+
+  const fetchGenreScore = async () => {
+    const result = axiosConfig.get(`/score/${userId}`);
+
+    return result;
+  };
+
+  const { isLoading: isGenreScoreLoading, data: genreScoreData } = useQuery({
+    queryKey: ['genreScore', userId],
+    queryFn: fetchGenreScore,
+  });
+
+  if (isGenreScoreLoading && isMyReviewLoading) return <Loading />;
+
+  if (!isGenreScoreLoading && !isMyReviewLoading) {
+    const { reviewCount, genreScore } = genreScoreData.data;
+    const { reviews } = myReviewData.data;
+    console.log(genreScore);
+    return (
+      <ResponsiveProvider
+        direction={'col'}
+        className={'gap-5 md:w-2/3 md:px-5 md:py-10 md:shadow-lg justify-start'}
+      >
+        <div className='flex flex-col justify-start w-full gap-5'>
+          <div className='flex justify-between'>
+            <LazyLoadImage
+              style={{ width: '150px', height: '150px' }}
+              className='rounded-full'
+              width={'150px'}
+              height={'150px'}
+              src='https://i.pinimg.com/564x/90/c9/b0/90c9b09c7152448fe3dd41d5dbea6dbb.jpg'
+              alt='profile-img'
+            />
+            <Button item={<SettingIcon />} className={'h-12 w-12 hover:text-violet-400'} />
+          </div>
+          <p className='text-xl'>{userInfo.displayName}</p>
+          <div className='flex gap-2'>
+            <div className='flex gap-1'>
+              <p>팔로워</p>
+              <span>10</span>
+            </div>
+            <div className='flex gap-1'>
+              <p>팔로잉</p>
+              <span>10</span>
+            </div>
+          </div>
+          <Button label={'팔로우'} bg={'main'} />
+          <div className='flex gap-3'>
+            <div className='text-center w-1/3'>
+              <p className='text-lg font-bold'>{reviewCount}</p>
+              <p className='border-violet-400'>리뷰</p>
+            </div>
+            <div className='text-center w-1/3'>
+              <p className='text-lg font-bold'>{reviewCount}</p>
+              <p>보고싶어요</p>
+            </div>
+            <div className='text-center w-1/3'>
+              <p className='text-lg font-bold'>{reviewCount}</p>
+              <p>봤어요</p>
+            </div>
+          </div>
+          <p>{userInfo.displayName} 님의 취향 점수</p>
+          <div className='flex flex-col gap-3'>
+            {!genreScore && <div>리뷰 데이터가 부족해요 🥲</div>}
+            {genreScore &&
+              genreScore.map((el) => {
+                return (
+                  <div key={el.genre_id} className='flex gap-2 justify-between'>
+                    <div className='flex items-center'>
+                      <p className=''>{el.genre_name}</p>
+                    </div>
+                    <div className='flex gap-2'>
+                      <p>
+                        <span className='text-sm text-violet-400'> 취향 점수</span> {el.score}점
+                      </p>
+                      <p>
+                        <span className='text-sm text-violet-400'> 리뷰 수</span>
+                        {el.count}개
+                      </p>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
         <CardWrapProvider
+          isHover={true}
           title={`${userInfo.displayName}님이 쓰신 리뷰`}
-          cardList={myReviewQuery.data.data.reviews}
+          cardList={reviews}
           onClick={redirectReview}
         />
-      )}
-    </React.Fragment>
-  );
+      </ResponsiveProvider>
+    );
+  }
 }
